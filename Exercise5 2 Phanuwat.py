4d31717dea1018162d5311365069f0b8efe0787c
MphBc <87320194+MphBc@users.noreply.github.com>
2021-12-31 21:41:53 +0700

    Add files via upload

---
+++ Exercise5 2 Phanuwat.py
@@ -0,0 +1,6 @@
+Distance = int(input("How far is it (km) ? :"))
+print(Distance,"km")
+Time = int(input("How long is it (hr) ? :"))
+print(Time,"hr")
+Velocity = Distance/Time
+print(Velocity,"km/hr")
