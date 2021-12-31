1f795d0b5a40447af04f69d4ea14976e270a3e52
MphBc <87320194+MphBc@users.noreply.github.com>
2021-12-31 11:59:27 +0700

    Add files via upload

---
+++ Exercise4_Phanuwat.py
@@ -0,0 +1,22 @@
+FE="Foundation English :"
+GB="General Business :"
+ICS="Introduction to Computer Systems :"
+CP="Computer Programming :"
+FEscore=20.0
+GBscore=10.0
+ICSscore=30.0
+CPscore=15.0
+print("---You score---")
+print(FE,FEscore)
+print(GB,GBscore)
+print(ICS,ICSscore)
+print(CP,CPscore)
+"""
+Test
+"""
+print("-----------------")
+print("Test type")
+print(type(FEscore))
+print(type(GBscore))
+print(type(ICSscore))
+print(type(CPscore))
